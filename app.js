134f15a329d72f1f1d2e6fb8848351267b204b2b
lucassodresa <lucas.sodresa@gmail.com>
2021-10-16 04:09:07 +0100

    modules applied

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+require("dotenv").config();
+const discord = require("./discord");
+const db = require("./db");
 
-app.get('/', (req, res) => {
-  res.send('Hello World! OSÓRIO E LUCAS!!!! #PARCEIROSDANOITE')
-})
+/* ## DISCORD ## 
+const channelID = "897251823264595979";
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-//commit directo
+const client = discord.login(process.env.BOT_TOKEN);
+
+discord.sendMessage(client, channelID, "message");
+*/
+
+/* ## SQLite DB Connection ## 
+
+*/
+
+(async () => {
+  await db.init();
+
+  //   await db.createMaintenance({
+  //     id: "post431247",
+  //     message: "muita ,messages, server down 333",
+  //     post_date: "2022-01-25 22:26:43.787 +00:00",
+  //     link: "https://google.com",
+  //     page: 20,
+  //   });
+
+  const result = await db.selectMostRecentPost();
+
+  console.log(result);
+  // await db.createMaintenance();
+})();
